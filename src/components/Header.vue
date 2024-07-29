0768974201544557c291591b93ce010c872bf421
SimoneDUrso <simone.durso98@gmail.com>
2024-07-29 17:06:02 +0200

    [UPD] Added v-for to cycle li in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,61 @@
 <script>
 export default {
-    
+    data(){
+        return{
+            navItems:[
+                {
+                    clicked: false,
+                    label: "CHARACTERS",
+                    url: "#",
+                },
+                {
+                    clicked: true,
+                    label: "COMICS",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "MOVIES",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "TV",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "GAMES",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "COLLECTIBLES",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "VIDEOS",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "FANS",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "NEWS",
+                    url: "#",
+                },
+                {
+                    clicked: false,
+                    label: "SHOP",
+                    url: "#",
+                },
+            ]
+        }
+    }
 }
 </script>
 <template>
@@ -10,16 +65,7 @@ export default {
                 <div class="col-12 d-flex align-items-center justify-content-between ">
                     <img src="../assets/dc-logo.png" alt="" class="mb-2">
                     <ul class="list-unstyled d-flex">
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">CHARACTERS</a></li>
-                        <li class="px-2"><a class="active text-decoration-none text-dark" href="#">COMICS</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">MOVIES</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">TV</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">GAMES</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">COLLECTIBLES</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">VIDEOS</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">FANS</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">NEWS</a></li>
-                        <li class="px-2"><a class="text-decoration-none text-dark" href="#">SHOP</a></li>
+                        <li v-for="item, index in navItems" :key="index" class="px-2"><a class="text-decoration-none " :href="item.url" :class="item.clicked ? 'active' : '' ">{{ item.label }}</a></li>
                     </ul>
                 </div>
             </div>
@@ -30,10 +76,18 @@ export default {
     ul{
         li{
             font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            a{
+                color: black;
+
+                &:hover{
+                    color: rgb(12, 124, 236);
+                }
+            }
 
             .active{
                 border-bottom: 5px solid rgb(12, 124, 236);
                 padding-bottom: 50px;
+                color: rgb(12, 124, 236);
             }
         }
     }
