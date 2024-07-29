b3c9397bd2f9edcbc7f478e362a0b895f887029d
SimoneDUrso <simone.durso98@gmail.com>
2024-07-29 16:43:06 +0200

    [UPD] Footer completed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,10 +4,28 @@ export default {
 }
 </script>
 <template>
-    <div>
-        
-    </div>
+    <footer class="position-relative">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 ps-0 mt-5 d-flex justify-content-between align-items-center fs-5">
+                    <button class="btn btn-lg border border-primary text-white">SIGN-UP NOW!</button>
+                    <div>
+                        <span class="text-primary mx-2 fw-bold">FOLLOW US</span>
+                        <span class="mx-2"><a href="#"><img src="../assets/footer-facebook.png" alt=""></a></span>
+                        <span class="mx-2"><a href="#"><img src="../assets/footer-twitter.png" alt=""></a></span>
+                        <span class="mx-2"><a href="#"><img src="../assets/footer-youtube.png" alt=""></a></span>
+                        <span class="mx-2"><a href="#"><img src="../assets/footer-pinterest.png" alt=""></a></span>
+                        <span class="mx-2"><a href="#"><img src="../assets/footer-periscope.png" alt=""></a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 <style lang="scss" scoped>
-    
+    footer{
+        background-color: #303030;
+        height: 150px;
+        z-index: 50;
+    }
 </style>
